411ac70633bedcdd9a8c103383cbb1db836b78ed
SCOSeanKly <48733997+SCOSeanKly@users.noreply.github.com>
2023-07-28 10:11:57 +0100

    Revert "changes"
    This reverts commit 7b624470ffebdb5881431cb012b447fb698dd86d.

--- Layer Elements (Controls)/Controls/Buttons + Sliders/SliderStepper.swift
+++ Layer Elements (Controls)/Controls/Buttons + Sliders/SliderStepper.swift
@@ -16,6 +16,7 @@ struct SliderStepper: View {
     @State private var isSliderMode = true
     @State private var buttonPressed = false
     @State private var timer: Timer?
+    @State private var sliderTypeOpacity: Bool = false
     
     let color: Color
     private var normalFillColor: Color { color.opacity(0.6) }
@@ -32,108 +33,124 @@ struct SliderStepper: View {
     
     
     var body: some View {
+        
         HStack {
-            if isSliderMode {
-                HStack {
-                    
-                    Text(title)
-                        .titleFont()
-                        .fontWeight(sliderBindingValue.wrappedValue == CGFloat(defaultValue) ? .regular : .semibold)
-                        .onTapGesture (count: 2) {
-                            feedback()
-                            sliderBindingValue.wrappedValue = CGFloat(defaultValue)
-                        }
-               
-                    CustomSlider(value: sliderBindingValue, inRange: CGFloat(minValue)...CGFloat(maxValue), activeFillColor: color, fillColor: normalFillColor, emptyColor: emptyColor, height: 8) { started in
-                    }
-                    .frame(height: 20)
-                    .padding(.leading, 5)
-                    
-                    
-                    Text("\(sliderBindingValue.wrappedValue, specifier: "%.\(specifier)f")")
-                        .specifierFont()
-                        .frame(width: 30)
-                        .offset(x: 10)
-                    
-                }
-            } else {
-                HStack {
-                    Text(title)
-                        .titleFont()
-                        .fontWeight(sliderBindingValue.wrappedValue == CGFloat(defaultValue) ? .regular : .semibold)
-                        .onTapGesture (count: 2) {
-                            sliderBindingValue.wrappedValue = CGFloat(defaultValue)
-                        }
-                    
-                    CustomSlider(value: sliderBindingValue, inRange: CGFloat(minValue)...CGFloat(maxValue), activeFillColor: color, fillColor: normalFillColor, emptyColor: emptyColor, height: 8) { started in
-                        
-                    }
-                    .frame(height: 20)
-                    .padding(.leading, 5)
-                    
-                    
+            
+            HStack {
+                if isSliderMode {
                     HStack {
-                        if Double(sliderBindingValue.wrappedValue) > minValue {
-                            Button(action: {
+                        
+                        Text(title)
+                            .titleFont()
+                            .fontWeight(sliderBindingValue.wrappedValue == CGFloat(defaultValue) ? .regular : .semibold)
+                            .onTapGesture (count: 2) {
                                 feedback()
-                                buttonPressed = true
-                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                                    buttonPressed.toggle()
-                                }
-                                let newValue = sliderBindingValue.wrappedValue - CGFloat(step)
-                                sliderBindingValue.wrappedValue = max(newValue, CGFloat(minValue))
-                                resetTimer()
-                            }) {
-                                Image(systemName: "chevron.left")
-                                    .fontWeight(.bold)
-                                    .padding(.horizontal, 10)
+                                sliderBindingValue.wrappedValue = CGFloat(defaultValue)
                             }
+                        
+                        CustomSlider(value: sliderBindingValue, inRange: CGFloat(minValue)...CGFloat(maxValue), activeFillColor: color, fillColor: normalFillColor, emptyColor: emptyColor, height: 8) { started in
                         }
+                        .frame(height: 20)
+                        .padding(.leading, 5)
                         
                         Text("\(sliderBindingValue.wrappedValue, specifier: "%.\(specifier)f")")
-                            .fontWeight(.semibold)
-                            .scaleEffect(buttonPressed ? 1.0 : 0.9)
-                            .animation(.interpolatingSpring(stiffness: 300, damping: 20), value: buttonPressed)
+                            .specifierFont()
+                            .frame(width: 30)
+                            .offset(x: 10)
+                    }
+                }
+                
+                else {
+                    HStack {
                         
-                        if Double(sliderBindingValue.wrappedValue) < maxValue {
-                            Button(action: {
-                                feedback()
-                                buttonPressed = true
-                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                                    buttonPressed.toggle()
+                        Text(title)
+                            .titleFont()
+                            .fontWeight(sliderBindingValue.wrappedValue == CGFloat(defaultValue) ? .regular : .semibold)
+                            .onTapGesture (count: 2) {
+                                sliderBindingValue.wrappedValue = CGFloat(defaultValue)
+                            }
+                        
+                        CustomSlider(value: sliderBindingValue, inRange: CGFloat(minValue)...CGFloat(maxValue), activeFillColor: color, fillColor: normalFillColor, emptyColor: emptyColor, height: 8) { started in
+                            
+                        }
+                        .frame(height: 20)
+                        .padding(.leading, 5)
+                        
+                        HStack {
+                            if Double(sliderBindingValue.wrappedValue) > minValue {
+                                Button(action: {
+                                    feedback()
+                                    buttonPressed = true
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                                        buttonPressed.toggle()
+                                    }
+                                    let newValue = sliderBindingValue.wrappedValue - CGFloat(step)
+                                    sliderBindingValue.wrappedValue = max(newValue, CGFloat(minValue))
+                                    resetTimer()
+                                }) {
+                                    Image(systemName: "chevron.left")
+                                        .fontWeight(.bold)
+                                        .padding(.horizontal, 10)
                                 }
-                                let newValue = sliderBindingValue.wrappedValue + CGFloat(step)
-                                sliderBindingValue.wrappedValue = min(newValue, CGFloat(maxValue))
-                                resetTimer()
-                            }) {
-                                Image(systemName: "chevron.right")
-                                    .fontWeight(.bold)
-                                    .padding(.horizontal, 10)
+                            }
+                            
+                            Text("\(sliderBindingValue.wrappedValue, specifier: "%.\(specifier)f")")
+                                .fontWeight(.semibold)
+                                .scaleEffect(buttonPressed ? 1.0 : 0.9)
+                                .animation(.interpolatingSpring(stiffness: 300, damping: 20), value: buttonPressed)
+                            
+                            if Double(sliderBindingValue.wrappedValue) < maxValue {
+                                Button(action: {
+                                    feedback()
+                                    buttonPressed = true
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                                        buttonPressed.toggle()
+                                    }
+                                    let newValue = sliderBindingValue.wrappedValue + CGFloat(step)
+                                    sliderBindingValue.wrappedValue = min(newValue, CGFloat(maxValue))
+                                    resetTimer()
+                                }) {
+                                    Image(systemName: "chevron.right")
+                                        .fontWeight(.bold)
+                                        .padding(.horizontal, 10)
+                                }
                             }
                         }
+                        .buttonStyle(PlainButtonStyle())
+                        .padding(.vertical, 5)
+                        .padding(.horizontal, 10)
+                        .fixedSize()
+                        .overlay(
+                            Capsule()
+                                .strokeBorder(lineWidth: 1, antialiased: true)
+                                .shadow(color: .black.opacity(0.25), radius: 3, x: 1, y: 1)
+                        )
+                        .clipShape(Capsule())
+                        .padding(.leading, 10)
                     }
-                    .buttonStyle(PlainButtonStyle())
-                    .padding(.vertical, 5)
-                    .padding(.horizontal, 10)
-                    .fixedSize()
-                    .overlay(
-                        Capsule()
-                            .strokeBorder(lineWidth: 1, antialiased: true)
-                            .shadow(color: .black.opacity(0.25), radius: 3, x: 1, y: 1)
-                    )
-                    .clipShape(Capsule())
-                    .padding(.leading, 10)
                 }
             }
-            
+            .animation(.easeInOut(duration: 0.1), value: isSliderMode)
+       
+            ///Button to toggle between Slider and Slider + Stepper
             Button(action: {
+                
                 feedback()
+                /*
+                sliderTypeOpacity.toggle()
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                    sliderTypeOpacity.toggle()
+                }
+                 */
+                
                 isSliderMode.toggle()
                 if !isSliderMode {
                     resetTimer()
                 } else {
                     cancelTimer()
                 }
+                
             }) {
                 Text(isSliderMode ? "\(Image(systemName: "chevron.right"))" : "\(Image(systemName: "xmark"))")
                     .foregroundColor(isSliderMode ? Color.primary : buttonColor)
@@ -143,7 +160,6 @@ struct SliderStepper: View {
         }
         .frame(height: 30)
         .padding(.horizontal)
-        .animation(.easeInOut(duration: 0.1), value: isSliderMode)
         .onAppear(perform: resetTimer) // Reset timer when the view appears
     }
     
