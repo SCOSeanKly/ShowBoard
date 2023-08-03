64cfb9addfbcc9cc01e0878974ba6162af5849fb
SCOSeanKly <48733997+SCOSeanKly@users.noreply.github.com>
2023-08-03 15:51:26 +0100

    Added object placeholders

---
+++ Layer Element Menu (Views)/ObjectSelectionButtons.swift
@@ -0,0 +1,269 @@
+//
+//  ObjectSelectionButtons.swift
+//  ShowBoard
+//
+//  Created by Sean Kelly on 03/08/2023.
+//
+
+import SwiftUI
+
+struct ObjectSelectionButton: View {
+    
+    var action: () -> Void
+    let imageType: ImageType
+    let textDescription: String
+   
+    
+    @State private var isPressing: Bool = false
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                ZStack {
+                    
+                    if let imageName = getImageName() {
+                        switch imageType {
+                        case .system:
+                            Image(systemName: imageName)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 30, height: 30)
+                                .scaleEffect(isPressing ? 0.8 : 1)
+                                .animation(.interpolatingSpring(stiffness: 300, damping: 10), value: isPressing)
+                            
+                        case .asset:
+                            Image(imageName)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 30, height: 30)
+                                .scaleEffect(isPressing ? 0.8 : 1)
+                                .animation(.interpolatingSpring(stiffness: 300, damping: 10), value: isPressing)
+                        }
+                    }
+                }
+                
+                Text(textDescription)
+                    .font(.caption)
+                    .foregroundColor(.primary)
+                    .padding(.top, -2)
+                    .frame(width: 50)
+                    .minimumScaleFactor(0.01)
+                    .lineLimit(1)
+                    .scaleEffect(isPressing ? 0.9 : 1)
+                    .animation(.interpolatingSpring(stiffness: 300, damping: 12), value: isPressing)
+            }
+        }
+        .buttonModifier(isPressing: isPressing)
+        .onTapGesture {
+            //MARK: Button Animation
+            isPressing.toggle()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                isPressing.toggle()
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    action()
+                }
+            }
+        }
+    }
+    
+    private func getImageName() -> String? {
+        switch imageType {
+        case .system(let name):
+            return name
+        case .asset(let name):
+            return name
+        }
+    }
+}
+
+struct ImportImageButton: View {
+    
+    let systemImage: String
+    let buttontext: String
+    @State private var isPressing: Bool = false
+    @Binding var buttonAction: Bool
+    @Binding var showLayerElementView: Bool
+    @Binding var importedImage: UIImage?
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                if let image = importedImage {
+                    Image(uiImage: image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 30, height: 30)
+                        .scaleEffect(isPressing ? 0.8 : 1)
+                        .animation(.interpolatingSpring(stiffness: 300, damping: 10), value: isPressing)
+                    
+                    
+                } else {
+                    Image(systemName: systemImage)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 30, height: 30)
+                        .scaleEffect(isPressing ? 0.8 : 1)
+                        .animation(.interpolatingSpring(stiffness: 300, damping: 10), value: isPressing)
+                    
+                }
+                
+                Text(buttontext)
+                    .font(.caption)
+                    .foregroundColor(.primary)
+                    .padding(.top, -2)
+                    .frame(width: 60)
+                    .lineLimit(1)
+                    .scaleEffect(isPressing ? 0.9 : 1)
+                    .animation(.interpolatingSpring(stiffness: 300, damping: 12), value: isPressing)
+                
+            }
+        }
+        .buttonModifier(isPressing: isPressing)
+        .onTapGesture {
+            isPressing.toggle()
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                isPressing.toggle()
+                
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    showLayerElementView = false
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                        buttonAction.toggle()
+                    }
+                    
+                }
+                
+            }
+        }
+    }
+}
+
+extension View {
+    func buttonModifier(isPressing: Bool) -> some View {
+        self.frame(width: 50, height: 60)
+            .padding(10)
+            .background(Color.white)
+            .cornerRadius(12)
+            .shadow(color: Color.black.opacity(isPressing ? 0.3 : 0.2),
+                    radius: isPressing ? 0.5 : 3,
+                    x: 0,
+                    y: isPressing ? 0 : 3)
+            .tint(.black)
+            .scaleEffect(isPressing ? 0.95 : 1)
+            .animation(.easeIn(duration: 0.1), value: isPressing)
+    }
+}
+
+struct ButtonsView: View {
+    @Binding var placedObjects: [LayerObject]
+    @Binding var selection: UUID?
+    
+    func removeSelectedObject() {
+        self.placedObjects.removeAll { $0.id == selection }
+    }
+    
+    func addObject(_ object: LayerObject) {
+        self.placedObjects.append(object)
+    }
+    
+    
+    var body: some View {
+        HStack {
+            Button(action: removeSelectedObject) {
+                Image(systemName: "trash")
+                    .tint(.red)
+            }
+            Spacer()
+            Button {
+                self.addObject(TextObject())
+            } label: {
+                Text("Text")
+            }
+            
+            Button {
+                self.addObject(CustomShapeObject())
+            } label: {
+                Text("Shape")
+            }
+            
+            Button {
+                self.addObject(CircleGaugeObject())
+            } label: {
+                Text("Gauge")
+            }
+            
+            Button {
+                self.addObject(MapObject())
+            } label: {
+                Text("Map")
+            }
+        }
+        .padding()
+    }
+}
+
+struct ObjectSelectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShowBoardView()
+    }
+}
+
+/*
+ 
+ ObjectTitleText(titleText: "Gauges")
+ 
+ LazyVGrid(columns: gridItems, spacing: 16) {
+ ObjectSelectionButton(imageType: .system(name: "barometer"), textDescription: "Battery")
+ }
+ .padding([.leading, .bottom, .trailing])
+ 
+ 
+ ObjectTitleText(titleText: "Weather")
+ 
+ LazyVGrid(columns: gridItems, spacing: 16) {
+ ObjectSelectionButton(imageType: .asset(name: "weather1"), textDescription: "Style 1")
+ ObjectSelectionButton(imageType: .asset(name: "weather2"), textDescription: "Style 2")
+ ObjectSelectionButton(imageType: .asset(name: "weather3"), textDescription: "Style 3")
+ ObjectSelectionButton(imageType: .asset(name: "weather4"), textDescription: "Style 4")
+ ObjectSelectionButton(imageType: .asset(name: "weather5"), textDescription: "Style 5")
+ ObjectSelectionButton(imageType: .asset(name: "weather6"), textDescription: "Style 6")
+ }
+ .padding([.leading, .bottom, .trailing])
+ 
+ ObjectTitleText(titleText: "Charts")
+ 
+ LazyVGrid(columns: gridItems, spacing: 16) {
+ ObjectSelectionButton(imageType: .system(name: "chart.bar"), textDescription: "Bar")
+ ObjectSelectionButton(imageType: .system(name: "chart.xyaxis.line"), textDescription: "Line")
+ ObjectSelectionButton(imageType: .system(name: "chart.pie"), textDescription: "Pie")
+ }
+ .padding([.leading, .bottom, .trailing])
+ 
+ 
+ ObjectTitleText(titleText: "Shapes")
+ 
+ LazyVGrid(columns: gridItems, spacing: 16) {
+ ObjectSelectionButton(imageType: .system(name: "rectangle"), textDescription: "Glass")
+ ObjectSelectionButton(imageType: .system(name: "star"), textDescription: "Custom")
+ 
+ 
+ }
+ .padding([.leading, .bottom, .trailing])
+ 
+ 
+ ObjectTitleText(titleText: "Calendar")
+ 
+ LazyVGrid(columns: gridItems, spacing: 16) {
+ ObjectSelectionButton(imageType: .system(name: "calendar"), textDescription: "Calendar")
+ 
+ ObjectSelectionButton(imageType: .system(name: "calendar.day.timeline.left"), textDescription: "Timeline")
+ 
+ }
+ .padding([.leading, .bottom, .trailing])
+ */
+
+
+
+
